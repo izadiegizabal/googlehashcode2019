f20e8a6a61a8eea565f09617effc8816e6daf96d
Izadi Egizabal Alkorta <64izadi@gmail.com>
2019-02-28 19:26:50 +0100

    Parser

--- src/Main.java
+++ src/Main.java
@@ -1,59 +1,37 @@
 import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
     private static String filename = "b_small";
 
     public static void main(String[] args) {
-        boolean[][] map = new boolean[0][];
-        int R, C, L, H;
+        int total;
+        ArrayList<Slide> unordered = new ArrayList<Slide>();
+        ArrayList<Photo> vertical = new ArrayList<Photo>();
 
         try (BufferedReader br = new BufferedReader(new FileReader("out/production/googlehashcode2019/" + filename + ".in"))) {
             String line;
             line = br.readLine();
 
-            String[] params = line.split("\\s+");
+            total = Integer.parseInt(line);
 
-            R = Integer.parseInt(params[0]);
-            C = Integer.parseInt(params[1]);
-            L = Integer.parseInt(params[2]);
-            H = Integer.parseInt(params[3]);
-
-            map = new boolean[R][C];
-
-            int lineCount = 0;
+            int indexRead = -1;
             while ((line = br.readLine()) != null) {
-                for (int i = 0; i < line.length(); i++) {
-                    map[lineCount][i] = line.charAt(i) == 'T'; // TOMATO = true, MUSHROOM = false
+                String [] params = line.split("\\s+");
+                ArrayList<String> tags = new ArrayList<>(Arrays.asList(params).subList(0, Integer.parseInt(params[1])));
+                if (params[0].equals("H")){
+                    unordered.add(new Slide(new ArrayList<Photo>(new Photo(indexRead, false, params))));
+                } else {
+                    vertical.add(new Photo(indexRead, true, params));
                 }
-                lineCount++;
+
+                indexRead++;
             }
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        StringBuilder output = new StringBuilder();
-        for (boolean[] booleans : map) {
-            for (boolean aBoolean : booleans) {
-                output.append(aBoolean).append(" ");
-            }
-            output.append("\n");
-        }
-        writeToOutput(output.toString());
-
-        String slices = getSlices(map);
-        // writeToOutput(slices);
-    }
-
-    private static String getSlices(boolean[][] map) {
-        StringBuilder slices = new StringBuilder();
-
-        for (boolean[] rows : map) {
-            for (int j = 0; j < rows.length; j++) {
-
-            }
-        }
-
-        return slices.toString();
     }
 
     private static void writeToOutput(String outputString) {
@@ -65,5 +43,9 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    private static void print(Object object) {
+        System.out.println(object);
     }
 }
